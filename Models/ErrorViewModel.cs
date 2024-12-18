0d6960b09040f8c48cc94f9b36fe60a2682358b4
furka <furkankesicioglu@hotmail.com>
2024-12-18 12:45:35 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace barbershop_web3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
