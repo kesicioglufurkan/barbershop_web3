de2b33a77d9fc6fb7b7d5f0fe9bd2442652b840f
kesicioglufurkan <furkanyildz2004@hotmail.com>
2024-12-25 23:31:19 +0300

    api consume added

---
+++ Controllers/SaloonApiController.cs
@@ -0,0 +1,37 @@
+﻿using barbershop_web3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace barbershop_web3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaloonApiController : ControllerBase
+    {
+        SaloonContext s = new SaloonContext();
+        // GET: api/<SaloonApiController>
+        [HttpGet]
+        public List<Saloon> Get()
+        {
+            var saloons = s.Saloons.ToList();
+
+            //return yazarlar;
+            return saloons;
+        }
+
+
+        // POST api/<SaloonApiController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Saloon sa)
+        {
+            s.Saloons.Add(sa);
+            s.SaveChanges();
+            return Ok(sa.SaloonName + " adlı salon eklendi");
+
+        }
+
+
+    }
+}
---
+++ Controllers/SaloonConsumeApiController.cs
@@ -0,0 +1,24 @@
+﻿using barbershop_web3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace barbershop_web3.Controllers
+{
+    public class SaloonConsumeApiController : Controller
+    {
+
+            SaloonContext s = new SaloonContext();
+            public async Task<IActionResult> Index()
+            {
+                List<Saloon> saloons = new List<Saloon>();
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync("https://localhost:7030/api/SaloonApi/");
+                var jsondata = await response.Content.ReadAsStringAsync();
+                saloons = JsonConvert.DeserializeObject<List<Saloon>>(jsondata);
+                return View(saloons);
+            }
+
+        }
+
+    
+}
